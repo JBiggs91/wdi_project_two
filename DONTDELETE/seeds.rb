510d0a616c78832088a1b31b7ce719edb7f89466
JBiggs91 <joe.biggica@gmail.com>
2014-10-07 16:07:55 -0400

    git fix

---
+++ DONTDELETE/seeds.rb
@@ -0,0 +1,16 @@
+require 'pry'
+require_relative './db/connection'
+require_relative './lib/category'
+require_relative './lib/contact'
+
+Category.delete_all
+Contact.delete_all
+
+friends = Category.create(name: "friends")
+frenemies = Category.create(name: "frenemies")
+other = Category.create(name: "other")
+
+Contact.create(name: "Sean", category_id: friends.id)
+Contact.create(name: "Jeff", category_id: friends.id)
+Contact.create(name: "Neel", category_id: frenemies.id)
+Contact.create(name: "Bob", category_id: other.id)
